b41acdc7a8f1e1f8abeb89e6291213123b115a5c
Charlie Liu <charlieliu2222@gmail.com>
2021-10-23 11:46:21 -0400

    New class structure for main script

--- posedetection.py
+++ posedetection.py
@@ -6,47 +6,55 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
-# For static images:
-IMAGE_FILES = ["./overload-jump-training.jpg"]
-BG_COLOR = (192, 192, 192) # gray
-with mp_pose.Pose(
-    static_image_mode=True,
-    model_complexity=2,
-    enable_segmentation=True,
-    min_detection_confidence=0.5) as pose:
-  for idx, file in enumerate(IMAGE_FILES):
-    image = cv2.imread(file)
-    image_height, image_width, _ = image.shape
-    # Convert the BGR image to RGB before processing.
-    results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+class pose_detection:
+  #put in the userImage and proImage
+  def __init__(self, userImage, proImage):
+    self.IMAGE_FILES = [userImage, proImage]
 
-    if not results.pose_landmarks:
-      continue
-    print(
-        f'Nose coordinates: ('
-        f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * image_width}, '
-        f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * image_height})'
-    )
+  def detect_pose(self):
+    # For static images:
+    BG_COLOR = (192, 192, 192) # gray
+    with mp_pose.Pose(
+        static_image_mode=True,
+        model_complexity=2,
+        enable_segmentation=True,
+        min_detection_confidence=0.5) as pose:
+      for idx, file in enumerate(self.IMAGE_FILES):
+        print(file)
+        image = cv2.imread(file)
+        image_height, image_width, _ = image.shape
+        # Convert the BGR image to RGB before processing.
+        results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-    annotated_image = image.copy()
-    # Draw segmentation on the image.
-    # To improve segmentation around boundaries, consider applying a joint
-    # bilateral filter to "results.segmentation_mask" with "image".
-    condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
-    bg_image = np.zeros(image.shape, dtype=np.uint8)
-    bg_image[:] = BG_COLOR
-    annotated_image = np.where(condition, annotated_image, bg_image)
-    # Draw pose landmarks on the image.
-    mp_drawing.draw_landmarks(
-        annotated_image,
-        results.pose_landmarks,
-        mp_pose.POSE_CONNECTIONS,
-        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-    cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
-    # Plot pose world landmarks.
-    mp_drawing.plot_landmarks(
-        results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
-    
+        if not results.pose_landmarks:
+          continue
+        print(
+            f'Nose coordinates: ('
+            f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * image_width}, '
+            f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * image_height})'
+        )
+
+        annotated_image = image.copy()
+        # Draw segmentation on the image.
+        # To improve segmentation around boundaries, consider applying a joint
+        # bilateral filter to "results.segmentation_mask" with "image".
+        condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
+        bg_image = np.zeros(image.shape, dtype=np.uint8)
+        bg_image[:] = BG_COLOR
+        annotated_image = np.where(condition, annotated_image, bg_image)
+        # Draw pose landmarks on the image.
+        mp_drawing.draw_landmarks(
+            annotated_image,
+            results.pose_landmarks,
+            mp_pose.POSE_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+        cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
+        # Plot pose world landmarks. PUT IN ANOTHER FUNCTION
+        # mp_drawing.plot_landmarks(
+        #     results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
+pd = pose_detection("./jump1.jpg", "./jump2.jpg")
+pd.detect_pose()
+
 
 #def video_upload_1 () : 
     # upload the first video 
